af24bfb84aefc1b47bb55f8570994e70ba522ba0
Adam Mesha <adam@mesha.org>
2014-11-23 17:29:37 +0200

    Fix the default include option in issue_query_patch.rb

--- lib/scrum/issue_query_patch.rb
+++ lib/scrum/issue_query_patch.rb
@@ -29,14 +29,14 @@ module Scrum
         alias_method_chain :initialize_available_filters, :scrum
 
         def issues_with_scrum(options = {})
-          options[:include] ||= {}
+          options[:include] ||= []
           options[:include] << :sprint
           issues_without_scrum(options)
         end
         alias_method_chain :issues, :scrum
 
         def issue_ids_with_scrum(options = {})
-          options[:include] ||= {}
+          options[:include] ||= []
           options[:include] << :sprint
           issue_ids_without_scrum(options)
         end
