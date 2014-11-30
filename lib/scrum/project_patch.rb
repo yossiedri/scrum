5a365b18f79ea704ef45216208983cb96b79d570
Adam Mesha <adam@mesha.org>
2014-11-30 14:24:15 +0200

    Allow sprints to be shared with subprojects
    Nearly identical logic to version sharing.
    Requires plugin migrations (rake redmine:plugins:migrate).

--- lib/scrum/project_patch.rb
+++ lib/scrum/project_patch.rb
@@ -15,6 +15,28 @@ module Scrum
           sprints.sort{|a, b| a.end_date <=> b.end_date}.last
         end
 
+        # Returns a scope of the Sprints used by the project
+        def shared_sprints
+	        if new_record?
+		        Sprint.
+			        includes(:project).
+			        where("#{Project.table_name}.status <> ? AND #{Sprint.table_name}.sharing = 'system'", STATUS_ARCHIVED)
+	        else
+		        @shared_sprints ||= begin
+                    r = root? ? self : root
+                    Sprint.
+                        includes(:project).
+                        where("#{Project.table_name}.id = #{id}" +
+                              " OR (#{Project.table_name}.status <> #{Project::STATUS_ARCHIVED} AND (" +
+                              " #{Sprint.table_name}.sharing = 'system'" +
+                              " OR (#{Project.table_name}.lft >= #{r.lft} AND #{Project.table_name}.rgt <= #{r.rgt} AND #{Sprint.table_name}.sharing = 'tree')" +
+                              " OR (#{Project.table_name}.lft < #{lft} AND #{Project.table_name}.rgt > #{rgt} AND #{Sprint.table_name}.sharing IN ('hierarchy', 'descendants'))" +
+                              " OR (#{Project.table_name}.lft > #{lft} AND #{Project.table_name}.rgt < #{rgt} AND #{Sprint.table_name}.sharing = 'hierarchy')" +
+                              "))")
+                end
+	        end
+        end
+
       end
     end
   end
